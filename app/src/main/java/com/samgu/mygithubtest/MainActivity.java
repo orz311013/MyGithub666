d2858eb633c3907556eaff3cedfa9c3090b63369
orz311013@gmail.com <a23925487>
2019-05-27 17:35:37 +0800

    v4

--- app/src/main/java/com/samgu/mygithubtest/MainActivity.java
+++ app/src/main/java/com/samgu/mygithubtest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.samgu.mygithubtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextClock;
 import android.widget.Toast;
 
@@ -12,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v("123","123");
-        Log.v("321","321");
-System.out.println("test");
 
     }
+
+    public void btn(View v) {
+        Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+    }
+
+    public void btn1(View v) {
+        Intent it = new Intent(this, Main2Activity.class);
+        startActivity(it);
+
+    }
+
 }
