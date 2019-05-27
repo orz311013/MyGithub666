e4e2864691284fd33967ff177ec76bad72b41967
orz311013@gmail.com <a23925487>
2019-05-27 17:21:37 +0800

    v3

--- app/src/main/java/com/samgu/mygithubtest/MainActivity.java
+++ app/src/main/java/com/samgu/mygithubtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.samgu.mygithubtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextClock;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-System.out.print("test");
+        Log.v("123","123");
+        Log.v("321","321");
+System.out.println("test");
+
     }
 }
